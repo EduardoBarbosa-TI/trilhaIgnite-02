a2fce88047ddbb54522db60dca4890cdb7420067
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-01-17 22:18:54 -0300

    Lidando com froent-end com TypeScript, React, TailwindCss, Postcss, autoprefixer

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import { Habit } from "./components/Habit"
+
+function App() {
+
+  return (
+    <>
+      <Habit completed={3}/>
+      <Habit completed={5}/>
+      <Habit completed={4}/>
+      <Habit completed={30}/>
+    </>
+    
+  )
+}
+
+export default App
+
+// Componente : Reaproveitar / isolar 
+// Propriedade: Uma informação enviada pra modificar um componente visual ou comportamental
